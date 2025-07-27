3366faf8681f30f1868385427124086c9941bb86
Harshit Soni <harshitashva@gmail.com>
2025-07-27 22:38:41 +0530

    fix: comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,9 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
+ 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-
+ 
 export default eslintConfig;
